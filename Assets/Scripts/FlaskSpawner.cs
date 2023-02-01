ce1b34000c60e52f13182491081b91da66b17c2f
Adrien ANTON LUDWIG <adrien.anton-ludwig@epita.fr>
2023-02-01 05:35:09 +0100

    Add flask spawner

---
+++ Assets/Scripts/FlaskSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlaskSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _flaskPrefab;
+    
+    private BoxCollider _flaskCollider;
+
+    public void SpawnFlask()
+    {
+        GameObject flask = Instantiate(_flaskPrefab, transform.position, transform.rotation);
+        _flaskCollider = flask.GetComponent<BoxCollider>();
+    }
+
+    // At the start of the game, spawn a flask
+    public void Start()
+    {
+        SpawnFlask();
+    }
+
+    // When the previous flask exits the spawner, spawn a new one
+    public void OnTriggerExit(Collider other)
+    {
+        if (other == _flaskCollider)
+            SpawnFlask();
+    }
+}
